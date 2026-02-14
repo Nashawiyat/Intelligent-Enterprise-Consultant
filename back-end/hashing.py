047a600c931e2fc469135bd765bb8e8c4ba2625b
Zuberi, Suhaib <sz2056@hw.ac.uk>
2026-02-14 16:08:27 +0800

    Added passwords hashing capabilities

--- back-end/hashing.py
+++ back-end/hashing.py
@@ -1,9 +1,11 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"])
 
 def hash_password(password):
+    password = password[:72]
     return pwd_context.hash(password)
 
 def verify_hash(password, hash):
+    password = password[:72]
     return pwd_context.verify(password, hash)
